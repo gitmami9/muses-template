c4a51e13b79c12bc698a53e0fe167f61832000a1
ameri8 <166777691+ameri8@users.noreply.github.com>
2024-05-11 16:39:24 +0900

    index.jsにコメント付けた

--- js-template/index.js
+++ js-template/index.js
@@ -1,15 +1,22 @@
-'use strict';
+"use strict";
 
-document.addEventListener('DOMContentLoaded', async () => {
+// スクリプトの実行をDOMContentLoadedイベント後に設定
+document.addEventListener("DOMContentLoaded", async () => {
+  // セッションストレージからユーザー名を取得
   const username = sessionStorage.username;
+  // ユーザー名が存在しない場合はログインページにリダイレクト
   if (!username) {
-    window.alert('ログインしてください');
-    location.href = 'login.html';
+    window.alert("ログインしてください");
+    location.href = "login.html";
   }
-  document.querySelector('#user_name span').textContent = username;
+  // ユーザー名を表示する要素にユーザー名をセット
+  document.querySelector("#user_name span").textContent = username;
 
-  const res = await fetch('data.json');
+  // データを取得して未読メッセージ数を表示
+  const res = await fetch("data.json");
   const obj = await res.json();
 
-  document.querySelectorAll('span.unread').forEach((el) => (el.textContent = obj.list.length));
+  document
+    .querySelectorAll("span.unread")
+    .forEach((el) => (el.textContent = obj.list.length));
 });
