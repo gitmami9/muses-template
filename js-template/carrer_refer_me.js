9d02c1215f90baebcf08c97ead0b8c41160a7364
gitmami9 <166770528+gitmami9@users.noreply.github.com>
2024-07-05 13:17:40 +0900

    活動記録表示

--- js-template/carrer_refer_me.js
+++ js-template/carrer_refer_me.js
@@ -1,32 +1,64 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // セッションストレージからユーザー名を取得
   const username = sessionStorage.username;
-  // ユーザー名が存在しない場合はログインページにリダイレクト
   if (!username) {
     window.alert("ログインしてください");
     location.href = "login.html";
+    return;
   }
-  // ユーザー名を表示する要素にユーザー名をセット
   document.querySelector("#user_name span").textContent = username;
 
-  // ユーザー名をキーにしてローカルストレージからデータを取得
   const userData = localStorage.getItem(username);
   if (userData) {
     const parsedData = JSON.parse(userData);
-    // 必要な情報を表示する部分にデータをセット
-    if (parsedData.length > 0) {
-      const latestData = parsedData[parsedData.length - 1];
-      document.getElementById("company").value = latestData.enterprise;
-      document.getElementById("industry_type").textContent =
-        latestData.industry_type;
-      document.getElementById("record").textContent = latestData.content;
-      document.getElementById(
-        "term"
-      ).textContent = `${latestData.term_sta} 〜 ${latestData.term_end}`;
-      document.getElementById("occupation").textContent = latestData.occupation;
-    }
+    populateDropdown(parsedData); // ドロップダウンリストを準備
   } else {
-    // データが存在しない場合の処理
     console.log("保存されたデータがありません");
   }
 });
+
+function populateDropdown(data) {
+  const select = document.getElementById("company");
+  data.forEach((item) => {
+    const opt = document.createElement("option");
+    opt.value = item.enterprise;
+    opt.textContent = item.enterprise;
+    select.appendChild(opt);
+  });
+
+  // 初期表示を設定するために最初の企業を選択
+  if (data.length > 0) {
+    const initialData = data[0]; // 最初のデータを初期表示として設定
+    updateDisplay(initialData);
+  }
+}
+
+function selectCompany(select) {
+  const username = sessionStorage.username;
+  const userData = localStorage.getItem(username);
+  if (userData) {
+    const parsedData = JSON.parse(userData);
+    const selectedCompany = select.value;
+    const selectedData = parsedData.find(
+      (item) => item.enterprise === selectedCompany
+    );
+    if (selectedData) {
+      updateDisplay(selectedData); // 選択された企業名に対応するデータを表示更新
+    }
+  }
+}
+
+function updateDisplay(data) {
+  document.getElementById("industry_type").textContent = data.industry_type;
+  document.getElementById("record").textContent = data.content;
+  document.getElementById(
+    "term"
+  ).textContent = `${data.term_sta} 〜 ${data.term_end}`;
+  document.getElementById("occupation").textContent = data.occupation;
+}
+function changeColor(select) {
+  if (select.value) {
+    select.style.color = "black";
+  } else {
+    select.style.color = "gray";
+  }
+}
