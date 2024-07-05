c901c18caca5986c08450e866496536b5e469c17
gitmami9 <166770528+gitmami9@users.noreply.github.com>
2024-07-05 09:57:59 +0900

    ローカルストレージに保存

--- js-template/carrer_enter.js
+++ js-template/carrer_enter.js
@@ -6,3 +6,37 @@ function changeColor(hoge) {
     hoge.style.color = "black";
   }
 }
+function saveToLocalStorage() {
+  const university = document.getElementById("university").value;
+  const faculty = document.getElementById("faculty").value;
+  const grade = document.getElementById("grade").value;
+  const content = document.getElementById("content").value;
+  const enterprise = document.getElementById("enterprise").value;
+  const industry_type = document.getElementById("industry_type").value;
+  const occupation = document.getElementById("occupation").value;
+  const term_sta = document.getElementById("term_sta").value;
+  const term_end = document.getElementById("term_end").value;
+
+  const formData = {
+    university,
+    faculty,
+    grade,
+    content,
+    enterprise,
+    industry_type,
+    occupation,
+    term_sta,
+    term_end,
+  };
+
+  const existingData = localStorage.getItem("careerFormData");
+  existingData = existingData ? JSON.parse(existingData) : [];
+
+  // 新しいデータを追加
+  existingData.push(formData);
+
+  // ローカルストレージに保存
+  localStorage.setItem("careerFormData", JSON.stringify(existingData));
+  alert("データが保存されました");
+  location.href = "carrer_menu.html";
+}
