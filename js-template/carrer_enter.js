4ee8ea732fbff4968797ef4ec20734ae52c88794
gitmami9 <166770528+gitmami9@users.noreply.github.com>
2024-07-05 11:05:11 +0900

    enter変更

--- js-template/carrer_enter.js
+++ js-template/carrer_enter.js
@@ -1,5 +1,3 @@
-/* ドロップダウンリストを選択すると、カラーを黒くする */
-
 document.addEventListener("DOMContentLoaded", async () => {
   // セッションストレージからユーザー名を取得
   const username = sessionStorage.username;
@@ -10,45 +8,46 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
   // ユーザー名を表示する要素にユーザー名をセット
   document.querySelector("#user_name span").textContent = username;
-  function changeColor(hoge) {
-    if (hoge.value == 0) {
-      hoge.style.color = "";
-    } else {
-      hoge.style.color = "black";
-    }
-  }
-  function saveToLocalStorage() {
-    const university = document.getElementById("university").value;
-    const faculty = document.getElementById("faculty").value;
-    const grade = document.getElementById("grade").value;
-    const content = document.getElementById("content").value;
-    const enterprise = document.getElementById("enterprise").value;
-    const industry_type = document.getElementById("industry_type").value;
-    const occupation = document.getElementById("occupation").value;
-    const term_sta = document.getElementById("term_sta").value;
-    const term_end = document.getElementById("term_end").value;
-
-    const formData = {
-      university,
-      faculty,
-      grade,
-      content,
-      enterprise,
-      industry_type,
-      occupation,
-      term_sta,
-      term_end,
-    };
-
-    let existingData = localStorage.getItem("careerFormData");
-    existingData = existingData ? JSON.parse(existingData) : [];
-
-    // 新しいデータを既存データに追加
-    existingData.push(formData);
-
-    // ローカルストレージに保存
-    localStorage.setItem("careerFormData", JSON.stringify(existingData));
-    alert("データが保存されました");
-    location.href = "carrer_menu.html";
-  }
 });
+/* ドロップダウンリストを選択すると、カラーを黒くする */
+function changeColor(hoge) {
+  if (hoge.value == 0) {
+    hoge.style.color = "";
+  } else {
+    hoge.style.color = "black";
+  }
+}
+function saveToLocalStorage() {
+  const university = document.getElementById("university").value;
+  const faculty = document.getElementById("faculty").value;
+  const grade = document.getElementById("grade").value;
+  const content = document.getElementById("content").value;
+  const enterprise = document.getElementById("enterprise").value;
+  const industry_type = document.getElementById("industry_type").value;
+  const occupation = document.getElementById("occupation").value;
+  const term_sta = document.getElementById("term_sta").value;
+  const term_end = document.getElementById("term_end").value;
+
+  const formData = {
+    university,
+    faculty,
+    grade,
+    content,
+    enterprise,
+    industry_type,
+    occupation,
+    term_sta,
+    term_end,
+  };
+
+  let existingData = localStorage.getItem("careerFormData");
+  existingData = existingData ? JSON.parse(existingData) : [];
+
+  // 新しいデータを既存データに追加
+  existingData.push(formData);
+
+  // ローカルストレージに保存
+  localStorage.setItem("careerFormData", JSON.stringify(existingData));
+  alert("データが保存されました");
+  location.href = "carrer_menu.html";
+}
