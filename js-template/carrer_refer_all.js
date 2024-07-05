fcf097aa418d9ec9aeb7af96df6900333a84c161
gitmami9 <166770528+gitmami9@users.noreply.github.com>
2024-07-05 11:10:40 +0900

    ログイン名表示

--- js-template/carrer_refer_all.js
+++ js-template/carrer_refer_all.js
@@ -16,3 +16,14 @@ window.addEventListener("DOMContentLoaded", (event) => {
   document.getElementById("ind").innerText = `${industryType}`;
   document.getElementById("date").innerText = `${termSta} ~ ${termEnd}`;
 });
+document.addEventListener("DOMContentLoaded", async () => {
+  // セッションストレージからユーザー名を取得
+  const username = sessionStorage.username;
+  // ユーザー名が存在しない場合はログインページにリダイレクト
+  if (!username) {
+    window.alert("ログインしてください");
+    location.href = "login.html";
+  }
+  // ユーザー名を表示する要素にユーザー名をセット
+  document.querySelector("#user_name span").textContent = username;
+});
--- js-template/carrer_refer_me.js
+++ js-template/carrer_refer_me.js
@@ -22,3 +22,14 @@ document.getElementById("company").addEventListener("change", function () {
     occupation.textContent = "職種";
   }
 });
+document.addEventListener("DOMContentLoaded", async () => {
+  // セッションストレージからユーザー名を取得
+  const username = sessionStorage.username;
+  // ユーザー名が存在しない場合はログインページにリダイレクト
+  if (!username) {
+    window.alert("ログインしてください");
+    location.href = "login.html";
+  }
+  // ユーザー名を表示する要素にユーザー名をセット
+  document.querySelector("#user_name span").textContent = username;
+});
